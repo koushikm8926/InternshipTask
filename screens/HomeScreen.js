d5c60e210c4bbd9dacb48b0fdb5debb12dc7bc56
Koushik Mondal <darkdeavil004@gmail.com>
2023-08-19 11:03:14 +0530

    search bar added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,35 @@
-import { Platform, StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { Platform, StyleSheet, Text, View,  TextInput, ScrollView } from 'react-native'
 import React from 'react'
+import { Feather } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={styles.TopView}>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
+    <ScrollView style={styles.ScrollView} showsVerticalScrollIndicator={false}>
+      <View style={styles.TopView}>
+        <TextInput
+          placeholder="Search for Restaurent item or more"
+          style={{ fontSize: 17, width: "90%" }}
+        />
+         <Feather name="search" size={24} color="#E52B50" />
+      </View>
+    </ScrollView>
   )
 }
 
 export default HomeScreen
 
 const styles = StyleSheet.create({
+  ScrollView:{
+    paddingTop: Platform.OS === "android" ? 35 : 0 
+  },
   TopView:{
-    marginTop:Platform.OS ==="android" ? 35:0 ,
+    flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          borderColor: "#C0C0C0",
+          borderWidth: 1,
+          padding: 10,
+          margin: 10,
+          borderRadius: 7,  
   }
 })
