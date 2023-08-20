acd9208f5dd0b0994791aa3a1936effad4a7950c
Koushik Mondal <darkdeavil004@gmail.com>
2023-08-20 10:28:55 +0530

    List of Food Added

---
+++ components/PopularFood.js
@@ -0,0 +1,96 @@
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
+import React from 'react'
+
+const PopularFood = () => {
+
+    const food=[
+        {
+            id:0,
+            name:"Jeera Rice",
+            description:'Jeera Bhaat or Jeera Rice is an Indian dish consisting of rice and cumin seeds.',
+            image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/xukq8swrwct8usmg4cjv",
+        },
+        {
+            id:"102",
+            name:"Chilly Chicken (Boneless)",
+            description:'Crispy and flavorful is how I would describe this Chilli Chicken. Boneless chicken is marinated in flavorful sauces, fried until crispy,',
+            image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ry3c3f518z10t4olu4l7",
+        },
+        {
+            id:1,
+            name:"Veg Fried Rice",
+            description:'Veg fried rice is a popular Asian recipe made with cooked rice stir-fried with various vegetables and seasoned with soy sauce',
+            image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/pycpbzawueci1dvhmkr3",
+      
+        },
+        {
+            id:"103",
+            name:"Spl Veg Biryani",
+            description:'Vegetable Biryani is an aromatic rice dish made with basmati rice, mix veggies, herbs & biryani spices',
+            image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/fsitbray4gq1kxcndqqx",
+        },
+        {
+            id:2,
+            name:"Egg Fried Rice",
+            description:'Egg fried rice recipe made in restaurant style, simple, quick and easy. The smoky aroma of the egg fried rice is great & delicious!!',
+            description:'This easy egg fried rice is a no-fuss weeknight meal that is better than takeout! ',
+            image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/lv6jl9qdscekjmwkxm9l",
+        },
+        {
+            id:"101",
+            name:"Paneer 65",
+            description:'Paneer 65 is a spicy South Indian appetizer made with Paneer aka Indian cheese, flour, spices and herbs like curry leaves.',
+            image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/druwjzmfmz7qvepq3bkr",
+        },
+        {
+            id:3,
+            name:"Chicken Fried Rice",
+            description:'Chicken fried rice is a take-out classic and an easy weeknight meal! ',
+            image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/akmx533z73jjbq8avy6v",
+        },
+        {
+            id:"104",
+            name:"Chilly Paneer",
+            description:'Chilli Paneer is an Indo-Chinese appetizer where crisp batter fried paneer is tossed in slightly sweet, spicy, hot and tangy chilli sauce.',
+            image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/byonwwb8mzxyqluxlqpq",
+        }
+    ]
+  return (
+    <View>
+      <Text style={styles.text}>Popular Food</Text>
+      <ScrollView >
+        {food.map((item, index) => (
+         
+         <View style={{ margin: 10, flexDirection:'row', }} key={index}>
+           
+            <Image source={{ uri: item.image }} style={{ height: 150,   borderRadius: 7,  width:120, }}/>
+                <View style={{flexDirection:'column',}}>
+                  
+                    <View style={{position: "absolute",left:10,}}>
+                        <Text style={{ marginTop: 6, textAlign: "center" ,fontSize:17,fontWeight: "600",}}>{item.name}</Text>
+                    </View>
+                    <View style={{marginTop:30,marginLeft:10,width:250,}}>
+                        <Text>{item.description}</Text>
+                    </View>
+
+            </View>
+          </View>
+       
+       ))}
+      </ScrollView>
+    </View>
+  )
+}
+
+export default PopularFood
+
+const styles = StyleSheet.create({
+    text:{
+        fontWeight:'bold',
+        fontSize:16,
+        margin:10,  
+    }
+})
+
+
+
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,6 +11,7 @@ import SearchFoodItems from '../data/SearchFoodItems';
 
 import HotelsFoodList from '../data/HotelsFoodList';
 import MenuItem from '../components/MenuItem';
+import PopularFood from '../components/PopularFood';
 
 const HomeScreen = () => {
   const [input, setInput]= useState("");
@@ -24,6 +25,11 @@ const HomeScreen = () => {
       image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/xukq8swrwct8usmg4cjv",
   },
   {
+      id:"102",
+      name:"Chilly Chicken (Boneless)",
+      image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ry3c3f518z10t4olu4l7",
+  },
+  {
       id:1,
       name:"Veg Fried Rice",
       description:'Veg fried rice is a popular Asian recipe made with cooked rice stir-fried with various vegetables and seasoned with soy sauce',
@@ -31,16 +37,35 @@ const HomeScreen = () => {
 
   },
   {
+      id:"103",
+      name:"Spl Veg Biryani",
+      description:'Vegetable Biryani is an aromatic rice dish made with basmati rice, mix veggies, herbs & biryani spices',
+      image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/fsitbray4gq1kxcndqqx",
+  },
+  {
       id:2,
       name:"Egg Fried Rice",
+      description:'Egg fried rice recipe made in restaurant style, simple, quick and easy. The smoky aroma of the egg fried rice is great & delicious!!',
       description:'This easy egg fried rice is a no-fuss weeknight meal that is better than takeout! ',
       image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/lv6jl9qdscekjmwkxm9l",
+  },
+  {
+      id:"101",
+      name:"Paneer 65",
+      description:'Paneer 65 is a spicy South Indian appetizer made with Paneer aka Indian cheese, flour, spices and herbs like curry leaves.',
+      image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/druwjzmfmz7qvepq3bkr",
   },
   {
       id:3,
       name:"Chicken Fried Rice",
       description:'Chicken fried rice is a take-out classic and an easy weeknight meal! ',
       image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/akmx533z73jjbq8avy6v",
+  },
+  {
+      id:"104",
+      name:"Chilly Paneer",
+      description:'Chilli Paneer is an Indo-Chinese appetizer where crisp batter fried paneer is tossed in slightly sweet, spicy, hot and tangy chilli sauce.',
+      image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/byonwwb8mzxyqluxlqpq",
   }
   ]
 
@@ -66,6 +91,7 @@ const HomeScreen = () => {
         <MenuItem key={index} item={item}/>
       ))}
 
+      <PopularFood/>
 
     </ScrollView>
   )
